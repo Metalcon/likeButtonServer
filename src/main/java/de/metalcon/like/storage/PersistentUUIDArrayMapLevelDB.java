2e7554093346a7259bedf226b8b0398614bf38f0
Jonas Kunze <jonas@metalcon.de>
2014-03-18 15:24:11 +0100

    Started with like button redesign

---
+++ src/main/java/de/metalcon/like/storage/PersistentUUIDArrayMapLevelDB.java
@@ -0,0 +1,89 @@
+package de.metalcon.like.storage;
+
+import java.util.HashMap;
+
+/**
+ * @author Jonas Kunze
+ */
+public class PersistentUUIDArrayMapLevelDB {
+	private static final int InitialArrayLength = 4;
+
+	LevelDBHandler dbHandler;
+
+	public PersistentUUIDArrayMapLevelDB(final long keyPrefix) {
+		dbHandler = new LevelDBHandler(keyPrefix);
+	}
+
+	/**
+	 * @return The timestamp of the last update
+	 */
+	public int getLastUpdateTimeStamp() {
+		return dbHandler.getInt("UpdateTS");
+	}
+
+	/**
+	 * 
+	 * @param updateTimeStamp
+	 *            The timestamp of the last update
+	 */
+	public void setUpdateTimeStamp(final int updateTimeStamp) {
+		dbHandler.put("UpdateTS", updateTimeStamp);
+	}
+
+	/**
+	 * 
+	 * @param keyUUID
+	 * @param valueUUID
+	 */
+	public void append(final long keyUUID, final long valueUUID) {
+		dbHandler.setAdd(keyUUID, valueUUID);
+	}
+
+	/**
+	 * Returns the long[] to which the keyUUID is mapped
+	 * 
+	 * @param keyUUID
+	 *            The key whose associated value is to be returned
+	 * @return the value to which the specified key is mapped, or null if this
+	 *         map contains no mapping for the key
+	 * @see HashMap#get(Object)
+	 */
+	public long[] get(final long keyUUID) {
+		return dbHandler.getLongs(keyUUID);
+	}
+
+	/**
+	 * Removes all elements associated with the given keyUUID
+	 * 
+	 * @param keyUUID
+	 *            The key of the list
+	 */
+	public void removeKey(final long keyUUID) {
+		dbHandler.removeKey(keyUUID);
+	}
+
+	/**
+	 * Removes the element valueUUID from the list associated with the given
+	 * keyUUID
+	 * 
+	 * @param keyUUID
+	 *            The key of the list
+	 * @param valueUUID
+	 *            The element to be deleted from the list
+	 */
+	public void remove(final long keyUUID, final long valueUUID) {
+		/*
+		 * TODO To be implemented
+		 */
+	}
+
+	/**
+	 * Will delete all elements in this map
+	 */
+	public void removeAll() {
+		/*
+		 * TODO To be implemented
+		 */
+	}
+
+}
